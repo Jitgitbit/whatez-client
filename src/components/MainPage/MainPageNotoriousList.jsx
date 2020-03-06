8326ee769ff2ab186f9ef7f6e8b60679598613b1
Jitgitbit <rowowolf@gmail.com>
2020-03-06 12:08:03 +0100

    initial components made

---
+++ src/components/MainPage/MainPageNotoriousList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MainPageNotoriousList() {
+  return (
+    <div>
+      <h1>List of most notorious additives</h1>
+      <p>----------------------------------</p>
+      <ul>
+        <li>E400</li>
+        <li>E450</li>
+      </ul>
+    </div>
+  )
+}
