859a7f70c2545d8c9ba1e079e659432665e416f6
Jitgitbit <rowowolf@gmail.com>
2020-03-19 19:27:17 +0100

    preparing for chart

--- src/components/ChartForAdditives.jsx
+++ src/components/ChartForAdditives.jsx
@@ -1,65 +1,8 @@
-// import * as React from 'react';
-// import Paper from '@material-ui/core/Paper';
-// import {
-//   Chart,
-//   PieSeries,
-//   Title,
-// } from '@devexpress/dx-react-chart-material-ui';
-
-// import { Animation } from '@devexpress/dx-react-chart';
-// import NavBar from './NavBar';
-
-// const data = [
-//   { E: 'E400', bought: 12 },
-//   { E: 'E450', bought: 7 },
-//   { E: 'E500', bought: 7 },
-//   { E: 'E160', bought: 7 },
-//   { E: 'E280', bought: 6 },
-//   { E: 'E301', bought: 5 },
-//   { E: 'E380', bought: 2 },
-//   { E: 'E270', bought: 55 },
-// ];
-// export default class ChartForAdditives extends React.PureComponent {
-  
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       data,
-//     };
-//   }
-
-//   render(props) {
-//     const { data: chartData } = this.state;
-//     console.log(props)
-
-//     return (
-//       <div>
-//       <NavBar/>
-//         <h1>What Ez</h1>
-//         <Paper style={{width: 500}}>
-//           <Chart
-//             data={chartData}
-//           >
-//             <PieSeries
-//               valueField="bought"
-//               argumentField="E"
-//             />
-//             <Title
-//               text="Ez consumed"
-//             />
-//             <Animation />
-//           </Chart>
-//         </Paper>
-//       </div>
-//     );
-//   }
-// }
-// export default connect(mapStateToProps, {getShots})(ChartForAdditives);
-
 import React, { Component } from "react";
 import { getShots } from "../actions/shotsActions";
 import { connect } from "react-redux";
+import PieChart from 'react-minimal-pie-chart';
+
 
 
 export class ChartForAdditives extends Component {
@@ -81,22 +24,30 @@ export class ChartForAdditives extends Component {
       return shot.arrayE
     })
     const allShotsMerged = [].concat.apply([], shotsList);
-    const additivesList = allShotsMerged.map((addi, index) => {
+    const additivesList = allShotsMerged.map((additive, index) => {
       return(
         <ul key={index}>
-          <li>{addi}</li>
+          <li>{additive}</li>
         </ul>
       )
+      // return additive;
     })
     // console.log('SHOTS in chartpage:',shotsList)
     console.log('ADDITIVES in chartpage:',allShotsMerged)
 
     return (
       <div>
-        <h1 style={{color:'green'}}>Ez listed</h1>
+        <h1 style={{color:'green'}}>Ez listed:</h1>
         <ul>
           {/* <li>{shotsList}</li> */}
-          <li>{additivesList}</li>
+          <li style={{marginLeft:'200px'}}>{additivesList}</li>
+          <PieChart
+          data={[
+            { title: 'One', value: 10, color: '#E38627' },
+            { title: 'Two', value: 15, color: '#C13C37' },
+            { title: 'Three', value: 20, color: '#6A2135' },
+          ]}
+          />;
         </ul>
       </div>
     );
