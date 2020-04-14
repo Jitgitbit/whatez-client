f00baf86fab53131c3e6edff73ed7619b6cc8eb8
Jitgitbit <rowowolf@gmail.com>
2020-04-14 17:19:38 +0200

    trying out different charts

--- src/components/ChartForAdditives.jsx
+++ src/components/ChartForAdditives.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { getShots } from "../actions/shotsActions";
 import { connect } from "react-redux";
 import PieChart from 'react-minimal-pie-chart';
-
-
+import Chart from 'chart.js';
+import Chartb from "./Chartb";
 
 export class ChartForAdditives extends Component {
 
@@ -11,7 +11,7 @@ export class ChartForAdditives extends Component {
     this.props.dispatch(getShots());
     // console.log(this.props)            // no shots yet at this point in time !
   }
-
+  
   render() {
     // console.log('SHOTS in chartpage:',this.props.shots)
     const allShotsRaw = this.props.shots;
@@ -34,6 +34,46 @@ export class ChartForAdditives extends Component {
     const randomColorOne = Math.floor(Math.random()*16777215).toString(16);
     const randomColorTwo = Math.floor(Math.random()*16777215).toString(16);
 
+    // const dataForGraph = (allShotsMerged) => {
+  
+      var integerAmount = [];
+      for (var i in allShotsMerged) {
+          integerAmount.push(allShotsMerged[i].CaseNum);
+      }
+      var sum = integerAmount.reduce((a, b) => a + b, 0);
+      var categoryname = ["E528", "E300", "E450", "E500", "E224", "E330", "E202"];
+      var category = [];
+      var amount = [];
+      for (var i in allShotsMerged) {
+          category.push(categoryname[allShotsMerged[i].Category]);
+          amount.push (integerAmount[i]/sum*100+'%');
+      }
+      var config = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: amount,
+                backgroundColor: [
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)",
+                    "rgba(59, 89, 152, 1)"
+                    ],
+                label: 'Dataset 1'
+            }],
+                labels: category
+        },
+        options: {
+            responsive: true
+        }
+    };
+    var ctx = ("#mycanvas");       
+    var graph = new Chart(ctx, config);
+    // }
+
     return (
       <div>
         <h1 style={{color:'green'}}>Ez listed:</h1>
@@ -46,8 +86,9 @@ export class ChartForAdditives extends Component {
             { title: 'Two', value: 15, color: '#' + randomColorTwo },
             { title: 'Three', value: 20, color: '#6A2135' },
           ]}
-          />;
+          />
         </ul>
+        <Chartb/>
       </div>
     );
   }
