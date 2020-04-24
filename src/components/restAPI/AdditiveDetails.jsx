c734217681eb97f9653f8c79619305abe7771448
Jitgitbit <rowowolf@gmail.com>
2020-04-24 15:55:39 +0200

    prettier add description

--- src/components/restAPI/AdditiveDetails.jsx
+++ src/components/restAPI/AdditiveDetails.jsx
@@ -9,11 +9,11 @@ export default function CategoryDetails(props) {
       <div className="card-content ">
         <h4 className="card-title red-text">E {props.additive.code}</h4>
         <br></br>
-        <p>name: {props.additive.name}</p>
-        <p>function: {props.additive.function}</p>
-        <p>food: {props.additive.foods}</p>
-        <p>notice: {props.additive.notice}</p>
-        <p>info: {props.additive.info}</p>
+        <h5>name</h5><p> {props.additive.name}</p>
+        <h5>function</h5><p> {props.additive.function}</p>
+        <h5>food examples</h5><p> {props.additive.foods}</p>
+        <h5>notice</h5><p className='red-text'> {props.additive.notice}</p>
+        <h5>extra info</h5><p> {props.additive.info}</p>
         <br></br>
         <button className="btn pink lighten-1 z-depth-0"><NavLink to="/additives" style={{color:'white'}}>back to list</NavLink></button>
         <br></br>
